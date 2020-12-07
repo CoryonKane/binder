2b6a60494e202e016613d5ab30442a2f9486b35d
coryon <illyes.gergely95@gmail.com>
2020-12-07 09:39:12 +0100

    project crud finished

--- src/main/java/com/codecool/binder/controller/ProjectController.java
+++ src/main/java/com/codecool/binder/controller/ProjectController.java
@@ -1,9 +1,11 @@
 package com.codecool.binder.controller;
 
+import com.codecool.binder.dto.ProjectDto;
+import com.codecool.binder.model.Project;
+import com.codecool.binder.model.User;
 import com.codecool.binder.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("project/")
@@ -13,5 +15,27 @@ public class ProjectController {
     @Autowired
     public ProjectController(ProjectService service) {
         this.service = service;
+    }
+
+    @GetMapping("{id}")
+    public ProjectDto getProject (@PathVariable("id") Long id) {
+        return service.getProject(id);
+    }
+
+    @PostMapping("")
+    public ProjectDto createProject (@RequestBody Project project) {
+        User sessionUser = null;
+        return service.saveProject(project, sessionUser);
+    }
+
+    @PutMapping("")
+    public ProjectDto updateProject (@RequestBody Project project) {
+        User sessionUser = null;
+        return service.saveProject(project, sessionUser);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteProject (@PathVariable("id") Long id) {
+        service.deleteProject(id);
     }
 }
--- src/main/java/com/codecool/binder/service/ProjectService.java
+++ src/main/java/com/codecool/binder/service/ProjectService.java
@@ -1,5 +1,8 @@
 package com.codecool.binder.service;
 
+import com.codecool.binder.dto.ProjectDto;
+import com.codecool.binder.model.Project;
+import com.codecool.binder.model.User;
 import com.codecool.binder.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,30 @@ public class ProjectService {
     @Autowired
     public ProjectService(ProjectRepository repository) {
         this.repository = repository;
+    }
+
+    public ProjectDto convert (Project p) {
+        return ProjectDto.builder()
+                .id(p.getId())
+                .url(p.getUrl())
+                .title(p.getTitle())
+                .owner(p.getOwner().getId())
+                .description(p.getDescription())
+                .build();
+    }
+
+    public ProjectDto getProject (Long id) {
+        Project p = repository.getOne(id);
+        return convert(p);
+    }
+
+    public ProjectDto saveProject(Project project, User sessionUser) {
+        project.setOwner(sessionUser);
+        repository.save(project);
+        return convert(project);
+    }
+
+    public void deleteProject(Long id) {
+        repository.deleteById(id);
     }
 }
