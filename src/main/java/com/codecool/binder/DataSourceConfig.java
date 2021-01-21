a8144c8c9a79712f1cea315883c0a8c3d7e9eed0
coryon <illyes.gergely95@gmail.com>
2021-01-21 12:11:40 +0100

    fix: spring data username and password changed to class variable

--- src/main/java/com/codecool/binder/DataSourceConfig.java
+++ src/main/java/com/codecool/binder/DataSourceConfig.java
@@ -14,12 +14,18 @@ public class DataSourceConfig {
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource postgresDataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
-        config.setUsername("${spring.datasource.username}");
-        config.setPassword("${spring.datasource.password}");
+        config.setUsername(username);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
 }
